2643dedff67ff33bf50d627a1704f514d2fc7342
Dimitris <crazyspirit1993@gmail.com>
2024-10-20 06:44:10 +0300

    b Commit

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
-from flask import Flask, render_template
-
+from flask import Flask, abort, render_template, redirect, url_for, flash, request
+from flask_bootstrap import Bootstrap5
+from flask_ckeditor import CKEditor
+from flask_login import UserMixin, login_user, LoginManager, current_user, logout_user
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship, DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Text
+from functools import wraps
+from werkzeug.security import generate_password_hash, check_password_hash
+import os
+from dotenv import load_dotenv
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return render_template("index.html")
 
+@app.route('/index')
+def index():
+    return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(debug=False, port=5002)
+    app.run(debug=False)
